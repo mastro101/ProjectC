ebb86629ae1e2b683c7ac43dc0e7f18a16193a16
EH MI_C_09 <nope>
2019-10-15 17:17:36 +0200

    Organizzazione cartelle

--- Assets/Combo/Scripts/CommandSequenceBase.cs
+++ Assets/Combo/Scripts/CommandSequenceBase.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu()]
 public class CommandSequenceBase : ScriptableObject
 {
     protected ICommandController controller;
--- Assets/Script/Input/CustomInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class CustomInput
-{
-    public static InputData southInput;
-    public static InputData estInput;
-    public static InputData westInput;
-    public static InputData northInput;
-}
