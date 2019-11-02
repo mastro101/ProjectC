ccf432664c3d9a280a62325c6ed27809b2257fbe
EasterMaster <teo.mastropasqua@gmail.com>
2019-11-02 18:13:13 +0100

    Creata scena di SelezioneScene + Feedback Slow Motion

--- Assets/UI/SlowMotion/Script/SlowMotionUI.cs
+++ Assets/UI/SlowMotion/Script/SlowMotionUI.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SlowMotionUI : MonoBehaviour
 {
-    [SerializeField] PlayerControllerInput player;
+    [SerializeField] PlayerData player;
+    [SerializeField] Image slider;
 
+    float remainTime;
 
+    private void Awake()
+    {
+        player.OnSlowMoUsed += UpdateSlowMotionTimer;
+    }
+
+    public void UpdateSlowMotionTimer(float _remainTime)
+    {
+        slider.fillAmount = (float)player.slowMoRemainTime / ((float)player.timeForSlowMo * player.slowMoPercent);
+    }
 }
