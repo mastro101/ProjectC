b6629d04bcd8d3317e0b6bb45ea56448fc75c1bf
Denis <serradenis2@live.it>
2020-01-23 12:51:06 +0100

    -Added split cinemachine

---
+++ Assets/Test_Cinemachine/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject virtualCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualCamera.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //virtualCamera.SetActive(false);
+    }
+}
